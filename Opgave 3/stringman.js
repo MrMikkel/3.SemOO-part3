c98167c8379b39375a3bb976c615b03187d65fcf
MrMikkel <79083834+MrMikkel@users.noreply.github.com>
2022-02-22 17:31:00 +0100

    Add files via upload

--- Opgave 3/stringman.js
+++ Opgave 3/stringman.js
@@ -27,8 +27,8 @@ Vue.createApp({
                 this.stringToManipulate="Det var ikke en string"
             }
             this.original=this.stringToManipulate;
-            this.lowercaser=this.stringToManipulate.toUpperCase();
-            this.uppercase=this.stringToManipulate.toLowerCase();
+            this.lowercaser=this.stringToManipulate.toLowerCase();
+            this.uppercase=this.stringToManipulate.toUpperCase();
             this.capitalized=this.stringToManipulate.charAt(0).toUpperCase() + this.stringToManipulate.slice(1).toLowerCase()
             this.endCapitalized=this.stringToManipulate.slice(0,-1).toLowerCase() + this.stringToManipulate.slice(-1).toUpperCase()
             this.reversed=this.stringToManipulate.split("").reverse().join("")
