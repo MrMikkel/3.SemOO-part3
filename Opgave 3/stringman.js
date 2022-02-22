8bf3c6af1d4298f25a91eeb4756ea5344d0ea9fb
MrMikkel <79083834+MrMikkel@users.noreply.github.com>
2022-02-22 17:24:46 +0100

    Add files via upload

---
+++ Opgave 3/stringman.js
@@ -0,0 +1,40 @@
+Vue.createApp({
+
+    data() {
+
+        return {
+
+            // add here
+
+            stringToManipulate: null,
+            original:null,
+            lowercaser:null,
+            uppercase:null,
+            capitalized:null,
+            endCapitalized:null,
+            reversed:null
+            
+
+        }
+
+    },
+
+    methods: {
+
+        // add here
+        manipulate(){
+            if (typeof this.stringToManipulate!="string") {
+                this.stringToManipulate="Det var ikke en string"
+            }
+            this.original=this.stringToManipulate;
+            this.lowercaser=this.stringToManipulate.toUpperCase();
+            this.uppercase=this.stringToManipulate.toLowerCase();
+            this.capitalized=this.stringToManipulate.charAt(0).toUpperCase() + this.stringToManipulate.slice(1).toLowerCase()
+            this.endCapitalized=this.stringToManipulate.slice(0,-1).toLowerCase() + this.stringToManipulate.slice(-1).toUpperCase()
+            this.reversed=this.stringToManipulate.split("").reverse().join("")
+
+        }
+
+    }
+
+}).mount("#app")
